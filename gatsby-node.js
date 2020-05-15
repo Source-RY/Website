db093929a0692917a38bd4710dc6febbdb7d996b
Janne Vanhanen <janne.j.v.vanhanen@icloud.com>
2020-05-15 13:32:42 +0300

    Add kebabification to event path

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const _ = require('lodash');
 
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
@@ -8,7 +9,6 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   // interpreter if not a single content uses it. Therefore, we're putting them
   // through `createNodeField` so that the fields still exist and GraphQL won't
   // trip up. An empty string is still required in replacement to `null`.
-
   switch (node.internal.type) {
   default: break;
   }
@@ -34,9 +34,8 @@ exports.createPages = async ({ graphql, actions }) => {
   `);
 
   for (const { node } of result.data.allStrapiEvent.edges) {
-
     createPage({
-      path: `/event/${node.name.toLowerCase()}`,
+      path: `/events/${_.kebabCase(node.name)}`,
       component: path.resolve('./src/templates/event.tsx'),
       context: {
         id: node.id
