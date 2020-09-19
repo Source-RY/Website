1820b11f1a455547beece22a12f96b76d6f5b44c
Janne Vanhanen <janne.vanhanen@outlook.com>
2020-09-19 17:03:31 +0300

    Updates

---
+++ component---src-pages-404-tsx-eb59b27b6998147458c3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"i6+/":function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t("Wbzz"),o=t("+ego");n.default=function(){return a.createElement(o.a,null,a.createElement("h1",{className:"not-found"},"Tais mennä väärin"),a.createElement("p",{className:"not-found"},"Typoista päätellen olet jo valmistunut insinööri!",a.createElement("br",null),a.createElement("br",null),a.createElement(l.a,{to:"/"},"Haluun pois")))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-eb59b27b6998147458c3.js.map
