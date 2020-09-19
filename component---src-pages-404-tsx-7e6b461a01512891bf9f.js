7d472fa153ed4371299b68b2b7bf67d30f655783
vanhanenjjv <vanhanenjjv@users.noreply.github.com>
2020-09-19 19:38:02 +0000

    Deploying to gh-pages from  @ f740a770f2ca5bab3cb0d2ff5a6194c344541ed5 🚀

---
+++ component---src-pages-404-tsx-7e6b461a01512891bf9f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"i6+/":function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t("Wbzz"),o=t("+ego");n.default=function(){return a.createElement(o.a,null,a.createElement("h1",{className:"not-found"},"Tais mennä väärin"),a.createElement("p",{className:"not-found"},"Typoista päätellen olet jo valmistunut insinööri!",a.createElement("br",null),a.createElement("br",null),a.createElement(l.a,{to:"/"},"Haluun pois")))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-7e6b461a01512891bf9f.js.map
