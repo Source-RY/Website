2cfe1d2871c40873e606df078bf1f3e6a9acb432
Janne Vanhanen <janne.vanhanen@outlook.com>
2021-08-22 00:09:23 +0300

    Restructure

--- website/gatsby-node.js
+++ website/gatsby-node.js
@@ -3,6 +3,8 @@ const path = require('path')
 const { pipe } = require('fp-ts/lib/function')
 const A = require('fp-ts/lib/Array')
 
+const kebabCase = require('kebab-case')
+
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
@@ -17,7 +19,7 @@ exports.createPages = async ({ graphql, actions }) => {
             description
           }
         }
-      }  
+      }
     }
   `)
 
@@ -28,10 +30,11 @@ exports.createPages = async ({ graphql, actions }) => {
   )
 
   function createEventPage ({ name, date, description }) {
-    console.log(name, date, description)
+    console.log(name)
+    console.log(kebabCase(name))
 
     createPage({
-      path: `/events/${name}`,
+      path: `/events/${kebabCase(name)}`,
       component: path.resolve('./src/templates/event.tsx'),
       context: {
         name,
