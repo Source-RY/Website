7c8d3d644263afada28af450fe65c460c6681199
Janne Vanhanen <janne.vanhanen@outlook.com>
2021-08-22 13:21:08 +0300

    savulohi

--- scripts/ci.js
+++
@@ -1,7 +0,0 @@
-import concurrently from 'concurrently'
-
-concurrently([
-  'cd ./src/strapi && npm ci',
-  'cd ./src/webhook-proxy && npm ci',
-  'cd ./src/gatsby && npm ci'
-])
--- scripts/dev.js
+++
@@ -1,7 +0,0 @@
-import concurrently from 'concurrently'
-
-concurrently([
-  'cd ./src/strapi && npm run start',
-  'cd ./src/webhook-proxy && npm run start',
-  'cd ./src/gatsby && npm run start'
-])
